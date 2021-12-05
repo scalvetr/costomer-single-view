ee6a339ad39ae5b612858e1499005efdf3e23de4
Sergi Calvet <sergi.calvet@gmail.com>
2021-12-05 21:01:12 +0100

    delete_schemas script

--- scripts/schema-registry.sh
+++ scripts/schema-registry.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-
 export CP_SCHEMA_REGISTRY_URL="http://localhost:8081"
 
-curl --request GET -sL \
-     --url ${CP_SCHEMA_REGISTRY_URL}/subjects | jq
+function delete_schemas() {
+    # shellcheck disable=SC2006
+    subjects=`curl --request GET --url ${CP_SCHEMA_REGISTRY_URL}/subjects`
+    for subject in $(echo "${subjects}" | jq -r '.[]'); do
+        curl --request DELETE -sL \
+             --url "${CP_SCHEMA_REGISTRY_URL}/subjects/${subject}"
+    done
+}
 
-curl --request DELETE -sL \
-     --url ${CP_SCHEMA_REGISTRY_URL}/subjects/event.customer.entity-key
 
-curl --request DELETE -sL \
-     --url ${CP_SCHEMA_REGISTRY_URL}/subjects/event.customer.entity-value
+delete_schemas;
+#curl --request DELETE -sL \
+#     --url ${CP_SCHEMA_REGISTRY_URL}/subjects/event.customer.entity-key
 
-curl --request DELETE -sL \
-     --url ${CP_SCHEMA_REGISTRY_URL}/subjects/topic-key
-
-curl --request DELETE -sL \
-     --url ${CP_SCHEMA_REGISTRY_URL}/subjects/topic-value
+#curl --request DELETE -sL \
+#     --url ${CP_SCHEMA_REGISTRY_URL}/subjects/event.customer.entity-value
