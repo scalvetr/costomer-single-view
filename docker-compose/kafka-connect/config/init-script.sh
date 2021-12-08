9201bc0bc376cf10ed9ebda11c778b56c7f47287
Sergi Calvet <sergi.calvet@gmail.com>
2021-12-08 19:56:07 +0100

    upgraded to cp-kafka-connect:7.0.0 and debezium connectors pre-installed

--- docker-compose/kafka-connect/config/init-script.sh
+++ docker-compose/kafka-connect/config/init-script.sh
@@ -4,3 +4,9 @@ echo "[INIT] confluent-hub install --no-prompt debezium/debezium-connector-postg
 confluent-hub install --no-prompt debezium/debezium-connector-postgresql:1.7.1
 echo "[INIT] confluent-hub install --no-prompt debezium/debezium-connector-mongodb:1.7.1"
 confluent-hub install --no-prompt debezium/debezium-connector-mongodb:1.7.1
+
+#
+echo "Launching Kafka Connect worker"
+/etc/confluent/docker/run &
+#
+sleep infinity
