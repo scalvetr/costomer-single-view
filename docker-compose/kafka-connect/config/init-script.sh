70544fb3a8c64a55471a2c105ed7f3b4e37e8b39
Sergi Calvet <sergi.calvet@gmail.com>
2021-12-08 21:55:34 +0100

    changed debezium mongo connector for "mongodb/kafka-connect-mongodb:1.6.1"

--- docker-compose/kafka-connect/config/init-script.sh
+++ docker-compose/kafka-connect/config/init-script.sh
@@ -2,9 +2,10 @@
 echo "[INIT] Installing Additional Connectors"
 echo "[INIT] confluent-hub install --no-prompt debezium/debezium-connector-postgresql:1.7.1"
 confluent-hub install --no-prompt debezium/debezium-connector-postgresql:1.7.1
-echo "[INIT] confluent-hub install --no-prompt debezium/debezium-connector-mongodb:1.7.1"
-confluent-hub install --no-prompt debezium/debezium-connector-mongodb:1.7.1
-
+#echo "[INIT] confluent-hub install --no-prompt debezium/debezium-connector-mongodb:1.7.1"
+#confluent-hub install --no-prompt debezium/debezium-connector-mongodb:1.7.1
+echo "[INIT] confluent-hub install --no-prompt mongodb/kafka-connect-mongodb:1.6.1"
+confluent-hub install --no-prompt mongodb/kafka-connect-mongodb:1.6.1
 #
 echo "Launching Kafka Connect worker"
 /etc/confluent/docker/run &
