9199c9da268a9f124380f0e83eb8982c5bed5733
Sergi Calvet <sergi.calvet@gmail.com>
2021-12-12 17:04:29 +0100

    04_kafka_connect_sinks.sql fix

--- config/ksqldb/03_processing.sql
+++ config/ksqldb/03_processing.sql
@@ -1,23 +1,23 @@
 -- CREATE a table
-CREATE STREAM event_core_banking_accounts
+CREATE STREAM "event_core_banking_accounts"
 AS
 SELECT *
-FROM raw_core_banking_accounts
+FROM "raw_core_banking_accounts"
     -- KEY = ACCOUNT_ID
     PARTITION BY ACCOUNT_ID;
 
 -- CREATE a table
-CREATE STREAM event_core_banking_bookings
+CREATE STREAM "event_core_banking_bookings"
 AS
 SELECT *
-FROM raw_core_banking_bookings
+FROM "raw_core_banking_bookings"
     -- KEY = ACCOUNT_ID
     PARTITION BY ACCOUNT_ID;
 
 -- CREATE a table
-CREATE STREAM event_contact_center_customer_cases
+CREATE STREAM "event_contact_center_customer_cases"
 AS
 SELECT *
-FROM raw_contact_center_customer_cases
+FROM "raw_contact_center_customer_cases"
     -- KEY = ACCOUNT_ID
     PARTITION BY CASE_ID;
