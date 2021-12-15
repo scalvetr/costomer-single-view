f9e38a3883fc58b68529bdacf6f0312bf38477f9
Sergi Calvet <sergi.calvet@gmail.com>
2021-12-16 00:01:34 +0100

    fixed connect sinks

--- config/ksqldb/02_kafka_connect_sinks.sql
+++ config/ksqldb/02_kafka_connect_sinks.sql
@@ -2,7 +2,7 @@ SHOW STREAMS;
 
 -- https://github.com/mongodb/mongo-kafka/blob/master/config/MongoSinkConnector.properties
 CREATE
-SINK CONNECTOR `customer-one-view-sink`
+SINK CONNECTOR `event_customer_entity-sink`
 WITH (
     'connection.uri'='mongodb://user:password@mongodb-customer-single-view/customer-single-view',
     'connector.class'='com.mongodb.kafka.connect.MongoSinkConnector',
@@ -12,39 +12,106 @@ WITH (
     'value.converter'='io.confluent.connect.avro.AvroConverter',
     'value.converter.schema.registry.url'='http://schema-registry:8081',
 
-    'key.projection.type'='none',
-    'key.projection.list'='',
-    'value.projection.type'='none',
-    'value.projection.list'='',
+    'topics'='event_customer_entity',
 
-    'topics'='event_customer_entity,event_core_banking_accounts,event_core_banking_bookings,event_contact_center_customer_cases',
+    -- Collection names
+    'collection'='customers',
 
     -- Id Strategy
     'post.processor.chain'='com.mongodb.kafka.connect.sink.processor.DocumentIdAdder',
     'document.id.strategy'='com.mongodb.kafka.connect.sink.processor.id.strategy.ProvidedInKeyStrategy', -- _id field in key
 
+    'transforms'='ValueToKey,RenameField',
+
+    'transforms.ValueToKey.type'='org.apache.kafka.connect.transforms.ValueToKey',
+    'transforms.ValueToKey.fields'='customerId',
+
+    'transforms.RenameField.type'='org.apache.kafka.connect.transforms.ReplaceField$Key',
+    'transforms.RenameField.renames'='customerId:_id'
+);
+
+CREATE
+SINK CONNECTOR `event_core_banking_accounts-sink`
+WITH (
+    'connection.uri'='mongodb://user:password@mongodb-customer-single-view/customer-single-view',
+    'connector.class'='com.mongodb.kafka.connect.MongoSinkConnector',
+    'database'='customer-single-view',
+
+    'key.converter'='org.apache.kafka.connect.storage.StringConverter',
+    'value.converter'='io.confluent.connect.avro.AvroConverter',
+    'value.converter.schema.registry.url'='http://schema-registry:8081',
+
+    'topics'='event_core_banking_accounts',
 
     -- Collection names
-    'collection'='customers',
-    'topic.override.event_core_banking_accounts.collection'='accounts',
-    'topic.override.event_core_banking_bookings.collection'='account_bookings',
-    'topic.override.event_contact_center_customer_cases.collection'='cases',
+    'collection'='accounts',
 
-    'transforms'='valueToKey,replaceField',
-    'transforms.valueToKey.type'='org.apache.kafka.connect.transforms.ValueToKey',
-    'transforms.replaceField.type'='org.apache.kafka.connect.transforms.ReplaceField$Key',
+    -- Id Strategy
+    'post.processor.chain'='com.mongodb.kafka.connect.sink.processor.DocumentIdAdder',
+    'document.id.strategy'='com.mongodb.kafka.connect.sink.processor.id.strategy.ProvidedInKeyStrategy', -- _id field in key
 
-    -- topic.override.event_customer_entity.
-    'transforms.valueToKey.fields'='customerId',
-    'transforms.replaceField.fields'='customerId:_id',
-    -- topic.override.event_core_banking_accounts.
-    'topic.override.event_core_banking_accounts.transforms.valueToKey.fields'='account_id',
-    'topic.override.event_core_banking_accounts.transforms.replaceField.fields'='account_id:_id',
-    -- topic.override.event_core_banking_bookings.
-    'topic.override.event_core_banking_bookings.transforms.valueToKey.fields'='booking_id',
-    'topic.override.event_core_banking_bookings.transforms.replaceField.fields'='booking_id:_id',
-    -- topic.override.event_contact_center_customer_cases.
-    'topic.override.event_contact_center_customer_cases.transforms.valueToKey.fields'='case_id',
-    'topic.override.event_contact_center_customer_cases.transforms.replaceField.fields'='case_id:_id'
+    'transforms'='ValueToKey,RenameField',
 
+    'transforms.ValueToKey.type'='org.apache.kafka.connect.transforms.ValueToKey',
+    'transforms.ValueToKey.fields'='account_id',
+
+    'transforms.RenameField.type'='org.apache.kafka.connect.transforms.ReplaceField$Key',
+    'transforms.RenameField.renames'='account_id:_id'
+);
+
+CREATE
+SINK CONNECTOR `event_core_banking_bookings-sink`
+WITH (
+    'connection.uri'='mongodb://user:password@mongodb-customer-single-view/customer-single-view',
+    'connector.class'='com.mongodb.kafka.connect.MongoSinkConnector',
+    'database'='customer-single-view',
+
+    'key.converter'='org.apache.kafka.connect.storage.StringConverter',
+    'value.converter'='io.confluent.connect.avro.AvroConverter',
+    'value.converter.schema.registry.url'='http://schema-registry:8081',
+
+    'topics'='event_core_banking_bookings',
+
+    -- Collection names
+    'collection'='bookings',
+
+    -- Id Strategy
+    'post.processor.chain'='com.mongodb.kafka.connect.sink.processor.DocumentIdAdder',
+    'document.id.strategy'='com.mongodb.kafka.connect.sink.processor.id.strategy.ProvidedInKeyStrategy', -- _id field in key
+
+    'transforms'='ValueToKey,RenameField',
+
+    'transforms.ValueToKey.type'='org.apache.kafka.connect.transforms.ValueToKey',
+    'transforms.ValueToKey.fields'='booking_id',
+
+    'transforms.RenameField.type'='org.apache.kafka.connect.transforms.ReplaceField$Key',
+    'transforms.RenameField.renames'='booking_id:_id'
+);
+CREATE
+SINK CONNECTOR `event_contact_center_customer_cases-sink`
+WITH (
+    'connection.uri'='mongodb://user:password@mongodb-customer-single-view/customer-single-view',
+    'connector.class'='com.mongodb.kafka.connect.MongoSinkConnector',
+    'database'='customer-single-view',
+
+    'key.converter'='org.apache.kafka.connect.storage.StringConverter',
+    'value.converter'='io.confluent.connect.avro.AvroConverter',
+    'value.converter.schema.registry.url'='http://schema-registry:8081',
+
+    'topics'='event_contact_center_customer_cases',
+
+    -- Collection names
+    'collection'='cases',
+
+    -- Id Strategy
+    'post.processor.chain'='com.mongodb.kafka.connect.sink.processor.DocumentIdAdder',
+    'document.id.strategy'='com.mongodb.kafka.connect.sink.processor.id.strategy.ProvidedInKeyStrategy', -- _id field in key
+
+    'transforms'='ValueToKey,RenameField',
+
+    'transforms.ValueToKey.type'='org.apache.kafka.connect.transforms.ValueToKey',
+    'transforms.ValueToKey.fields'='case_id',
+
+    'transforms.RenameField.type'='org.apache.kafka.connect.transforms.ReplaceField$Key',
+    'transforms.RenameField.renames'='case_id:_id'
 );
